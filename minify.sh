1904ea0888bd942c2c0c92660530ce868aa0d4d9
mustilica <ilhan.mstf@gmail.com>
2016-04-16 14:48:32 +0300

    local variables and new minify rule

--- minify.sh
+++ minify.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 pushd css
-cat bootstrap.min.css font.css style.css > all.css
-java -jar ../../yuicompressor/yuicompressor-2.4.8.jar all.css -o all.min.css
+cat bootstrap.min.css font.css style.css base64images.css > all.css
+java -jar ../../yuicompressor/yuicompressor-2.4.7.jar all.css -o all.min.css
 rm all.css
 popd
 
 pushd js
-cat googleAnalytics.js jquery-1.11.3.min.js tweenLite.min.js easePack.min.js raf.js networkGraphAnimation.js jquery.simpleWeather-3.0.2.min.js weather.js script.js > all.js
-java -jar ../../yuicompressor/yuicompressor-2.4.8.jar all.js -o all.min.js
+#cat googleAnalytics.js jquery-1.12.3.min.js tweenLite.min.js easePack.min.js raf.js networkGraphAnimation.js jquery.simpleWeather.min.js weather.js script.js > all.js
+cat googleAnalytics.js jquery-1.12.3.min.js jquery.simpleWeather.min.js weather.js script.js > all.js
+java -jar ../../yuicompressor/yuicompressor-2.4.7.jar all.js -o all.min.js
 rm all.js
 popd
