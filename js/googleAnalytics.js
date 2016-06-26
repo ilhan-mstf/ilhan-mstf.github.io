b9bca6e23c011865174e9204f0c215961b1b8e55
mustilica <ilhan.mstf@gmail.com>
2016-06-26 19:18:29 +0300

    analytics script update, removed unused feature

--- js/googleAnalytics.js
+++ js/googleAnalytics.js
@@ -11,6 +11,5 @@
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-37257657-1', 'auto');
-ga('require', 'displayfeatures');
 ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
